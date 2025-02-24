373a6bc5c8e76bbb2c2b489c823418feba3c9054
bruker132 <bakriaa@stud.ntnu.no>
2025-02-24 08:19:21 +0100

    Added MovieTicketClient class to handle individual ticket booking requests using threads.

--- MovieTicket/src/booking/MovieTicketClient.java
+++ MovieTicket/src/booking/MovieTicketClient.java
@@ -1,5 +1,19 @@
 package booking;
 
-public class MovieTicketClient {
+public class MovieTicketClient implements Runnable {
+    private String customerName;
+    private int ticketsToBook;
+    private MovieTicketServer server;
 
+    MovieTicketClient(String customerName, int ticketsToBook, MovieTicketServer server) {
+        this.customerName = customerName;
+        this.ticketsToBook = ticketsToBook;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        server.bookTickets(customerName, ticketsToBook);
+
+    }
 }
