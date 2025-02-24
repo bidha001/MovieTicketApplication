86db12599086df294dd085722a9dea053116919b
bruker132 <bakriaa@stud.ntnu.no>
2025-02-24 08:10:40 +0100

    Implemented thread movie ticket using synchronized to ensure correct ticket.

--- MovieTicket/src/booking/MovieTicketServer.java
+++ MovieTicket/src/booking/MovieTicketServer.java
@@ -1,4 +1,25 @@
 package booking;
 
 public class MovieTicketServer {
+    private String movieName;
+    private int availableTickets;
+
+    MovieTicketServer(String movieName, int availableTickets) {
+        this.movieName = movieName;
+        this.availableTickets = availableTickets;
+    }
+
+    public synchronized boolean bookTickets(String customerName, int tickets) {
+        if (tickets > availableTickets) {
+            return false;
+        } else {
+        availableTickets -= tickets;
+        System.out.println("Tickets booked for " + customerName + " for " + movieName + " for " + tickets + " tickets.");
+        return true;
+    }
+}
+
+    public int getAvailableTickets () {
+        return availableTickets;
+    }
 }
